2cd32b857fb341932351f40ca293ec75faeaea35
Mateusz Nowak <mateusz.nowak@swmansion.com>
2018-03-25 22:42:39 +0200

    First implementation of Rastrigin problem

--- src/main/java/pl/agenty/App.java
+++ src/main/java/pl/agenty/App.java
@@ -1,13 +1,66 @@
 package pl.agenty;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
+import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
+import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.problem.singleobjective.Rastrigin;
+import org.uma.jmetal.runner.multiobjective.NSGAIIRunner;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+
+public class App extends NSGAIIRunner
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws FileNotFoundException {
+
+        Problem<DoubleSolution> problem;
+        Algorithm<List<DoubleSolution>> algorithm;
+        CrossoverOperator<DoubleSolution> crossover;
+        MutationOperator<DoubleSolution> mutation;
+        SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
+
+        int numberOfVariables = 100;
+
+        problem = new Rastrigin(numberOfVariables);
+
+        double crossoverProbability = 0.9 ;
+        double crossoverDistributionIndex = 20.0 ;
+        crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex) ;
+
+        double mutationProbability = 1.0 / problem.getNumberOfVariables() ;
+        double mutationDistributionIndex = 20.0 ;
+        mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex) ;
+
+        selection = new BinaryTournamentSelection<DoubleSolution>(new RankingAndCrowdingDistanceComparator<DoubleSolution>());
+
+        algorithm = new NSGAIIBuilder<DoubleSolution>(problem, crossover, mutation)
+                .setSelectionOperator(selection)
+                .setMaxEvaluations(2500000)
+                .setPopulationSize(100)
+                .build() ;
+
+        AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+                .execute() ;
+
+        List<DoubleSolution> population = algorithm.getResult() ;
+        long computingTime = algorithmRunner.getComputingTime() ;
+
+        JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+
+        printFinalSolutionSet(population);
+
     }
 }
